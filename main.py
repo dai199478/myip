706b973753745bf85d80d7e94ac9046a9dd8fa16
dhh78@qq.com <dhh78@qq.com>
2018-11-21 00:00:32 +0800

    add ip and py file

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import re
+
+
+def get_ip_by_ip138():
+    response = requests.get("http://2018.ip138.com/ic.asp")
+    ip = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", response.content.decode(errors='ignore')).group(0)
+    return ip
+
+
+if __name__ == '__main__':
+    ip = get_ip_by_ip138()
+    print("本机的ip地址为:", ip)
+    open('ip.txt', 'w').write(ip)
